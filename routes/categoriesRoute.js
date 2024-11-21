026c8a3b23c67d77f1c9e9bbbfaf832ef1da3ad5
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 21:20:35 +0530

    Import and call getCategory function from route file

--- routes/categoriesRoute.js
+++ routes/categoriesRoute.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { postCategory } from '../controllers/categoryControllers.js'
+import { getCategory, postCategory } from '../controllers/categoryControllers.js'
 
 const categoryRouter = express.Router()
 
 categoryRouter.post("/",postCategory)
+
+categoryRouter.get("/",getCategory)
 
 export default categoryRouter;
 
