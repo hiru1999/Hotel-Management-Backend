276112407ec11e90c17255b7af2d2aa98d1f544f
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 20:52:44 +0530

    Create category model

---
+++ models/category.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const categorySchema = mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        price : {
+            type : Number,
+            required : true
+        },
+        features : [
+            {
+                type : String
+            }
+        ],
+        description : {
+            type : String,
+            required : true
+        },
+        image : {
+            type : String
+        }
+    }
+)
