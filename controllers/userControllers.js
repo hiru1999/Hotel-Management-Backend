50095adc3f3370542bc324def34f394e3a7aa355
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 18:46:39 +0530

    Create password hashing

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,17 @@
 import User from "../models/user.js";
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 //post
 export function postUsers(req, res) {
     const user = req.body;
+    const password = req.body.password
+
+    const saltRounds = 10;
+    const passwordHash = bcrypt.hashSync(password,saltRounds);
+    console.log(passwordHash)
+    user.password = passwordHash
+
     const newUser = new User(user);
 
     newUser.save().then(
@@ -56,26 +64,35 @@ export function deleteUsers(req,res){
 //login
 export function loginUser(req,res){
     const credentials = req.body
-    User.findOne({email : credentials.email, password : credentials.password}).then(
+   
+    User.findOne({email : credentials.email}).then(
         (user)=>{
             if(user == null){
                 res.json({
                     message : "User not found"
                 })
             }else{
-                const payload = {
-                    id: user._id,
-                    email : user.email,
-                    firstName : user.firstName,
-                    lastName : user.lastName,
-                    type : user.type
-                };
-                const token = jwt.sign(payload, "secret", {expiresIn : "48h"});
-                res.json({
-                    message : "User found",
-                    user : user,
-                    token : token
-                })
+                const isPasswordValid = bcrypt.compareSync(credentials.password, user.password);
+                if(!isPasswordValid){
+                    res.json({
+                        message : "Incorrect password"
+                    });
+                }else{
+                    const payload = {
+                        id: user._id,
+                        email : user.email,
+                        firstName : user.firstName,
+                        lastName : user.lastName,
+                        type : user.type
+                    };
+                    const token = jwt.sign(payload, "secret", {expiresIn : "48h"});
+                    res.json({
+                        message : "User found",
+                        user : user,
+                        token : token
+                    })
+                }
+                
             }
         }
     )
