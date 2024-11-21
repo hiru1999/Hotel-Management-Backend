a6323a52a7ba69152a706bb4c0058ad79eb17b32
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 23:11:06 +0530

    Create postRoom function

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,2 +1,33 @@
 import Room from "../models/room";
+import { isAdminValid } from "./userControllers.js";
 
+//post
+export function postRoom(req,res){
+    
+    if(!isAdminValid(req)){
+        res.status(403).json({
+            message : "Unauthorized"
+        })
+        return
+    }
+
+    const room = req.body
+    const newRoom = new Room(room)
+
+    newRoom.save().then(
+        (result)=>{
+            res.json({
+                message : "Room created successfully",
+                result : result
+            })
+        }
+    ).catch(
+        (err)=>{
+            res.json({
+                message : "Room creation failed",
+                error : err
+                
+            })
+        }
+    )
+}
