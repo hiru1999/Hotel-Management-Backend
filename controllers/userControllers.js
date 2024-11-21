600fe045c9955e21faea1eac67d116875721fd85
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 19:54:05 +0530

    Install dotenv and create keys

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,9 @@
 import User from "../models/user.js";
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 //post
 export function postUsers(req, res) {
@@ -85,7 +88,8 @@ export function loginUser(req,res){
                         lastName : user.lastName,
                         type : user.type
                     };
-                    const token = jwt.sign(payload, "secret", {expiresIn : "48h"});
+                    const token = jwt.sign(payload, process.env.JWT_KEY, {expiresIn : "48h"});
+                    
                     res.json({
                         message : "User found",
                         user : user,
