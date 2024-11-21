aa12802376b894816b797a46d45968ca826f9fbb
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 13:23:31 +0530

    Create login authorization

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ import express from 'express'
 import userRouter from './routes/usersRoute.js'
 import mongoose from 'mongoose'
 import galleryItemRouter from './routes/galleryItemsRoute.js'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 
 app.use(bodyParser.json())
 
 const connectionString = "mongodb+srv://tester2:321@cluster0.kttso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+app.use((req,res,next)=>{
+    const token = req.header("Authorization")?.replace("Bearer","")
+    if(token != null){
+        jwt.verify(token,"secret",(err,decoded)=>{
+            if(decoded!=null){
+                req.body.user  = decoded
+                next()
+            }else{
+                next()
+            }  
+        })
+    }else{
+        next()
+    }
+});
 
 mongoose.connect(connectionString).then(
     ()=>{
