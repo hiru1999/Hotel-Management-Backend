7a13336f4f1806d60b53866bf101640415eef3b8
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-21 21:09:48 +0530

    Import and call postCategory function from route file

--- routes/categoriesRoute.js
+++ routes/categoriesRoute.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { postCategory } from '../controllers/categoryControllers.js'
 
 const categoryRouter = express.Router()
+
+categoryRouter.post("/",postCategory)
 
 
 
