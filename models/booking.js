0099b164bdac36a54609cb7d883d579a63789d8a
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 08:02:34 +0530

    Create roomSchema

---
+++ models/booking.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const bookingSchema = mongoose.Schema(
+    {
+        bookingId : {
+            type : Number,
+            required : true,
+            unique : true
+        },
+        email : {
+            type : String,
+            required : true
+        },
+        status : {
+            type : String,
+            required : true,
+            default : "pending"
+        },
+        reason : {
+            type : String,
+            default : ""
+        },
+        start : {
+            type : Date,
+            required : true
+        },
+        end : {
+            type : Date,
+            required : true
+        },
+        notes : {
+            type : String,
+            default : ""
+        },
+    }
+)
