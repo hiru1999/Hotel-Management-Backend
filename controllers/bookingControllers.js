c64802dcce405bc562ee69acced976dfa625274d
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 08:34:44 +0530

    Create and export postBooking function

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -1,11 +1,59 @@
 import Booking from "../models/booking.js";
-import { isAdminValid } from "./userControllers.js";
+import { isCustomerValid } from "./userControllers.js";
+
 
 
 //post
 export function postBooking(req,res){
 
-    
+    if(!isCustomerValid(req)){
+        res.status(403).json({
+            message : "Unauthorized"
+        })
+        return
+    }
+
+    const startingId = 1200;
+
+    Booking.countDocuments({}).then(
+        (count)=>{
+            console.log(count);
+            const newId = startingId + count + 1;
+            const newBooking = new Booking(
+                {
+                    bookingId : newId,
+                    roomId : req.body.roomId,
+                    email : req.user.email,
+                    start : req.body.start,
+                    end : req.body.end
+                }
+            )
+            newBooking.save().then(
+                (result)=>{
+                    res.json({
+                        message : "Booking created successfully",
+                        result : result
+                    })
+                }
+            ).catch(
+                (err)=>{
+                    res.json({
+                        message : "Booking creation failed",
+                        error : err
+                        
+                    })
+                }
+            )
+        }
+    ).catch(
+        (err)=>{
+            res.json({
+                message : "Booking creation failed",
+                error : err
+                
+            })
+        }
+    )
 
 }
 
