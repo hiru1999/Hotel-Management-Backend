407ef2f5e637a2d946887905e5556bc02846ed2a
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-22 09:40:04 +0530

    Create and export postFeedback function

--- controllers/feedbackControllers.js
+++ controllers/feedbackControllers.js
@@ -1,1 +1,32 @@
 import Feedback from "../models/feedback.js";
+
+//post
+export function postFeedback(req,res){
+
+    if(!isCustomerValid(req)){
+        res.status(403).json({
+            message : "Unauthorized"
+        })
+        return
+    }
+
+    const feedback = req.body
+    const newFeedback = new Feedback(feedback)
+
+    newFeedback.save().then(
+        (result)=>{
+            res.json({
+                message : "Feedback sent successfully",
+                result : result
+            })
+        }
+    ).catch(
+        (err)=>{
+            res.json({
+                message : "Feedback sent failed",
+                error : err
+            })
+        }
+    )
+
+}
