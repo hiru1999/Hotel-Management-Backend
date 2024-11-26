093e25fb6db7bf3b2b11506c7c7e8409b74a1c69
hiru1999 <hiruvisenadeera@gmail.com>
2024-11-26 22:51:27 +0530

    Call getUser from usersRoute

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { deleteUsers, getUsers, loginUser, postUsers } from '../controllers/userControllers.js'
+import { deleteUsers, getUser, getUsers, loginUser, postUsers } from '../controllers/userControllers.js'
 
 
 const userRouter = express.Router()
 
+userRouter.get("/",getUser)
 userRouter.get("/",getUsers)
 
 userRouter.post("/",postUsers)
